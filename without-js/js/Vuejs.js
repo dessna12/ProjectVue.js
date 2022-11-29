87c6e034744191293a2218d9cef61149fc243085
dessna12 <92819503+dessna12@users.noreply.github.com>
2022-11-29 09:58:28 +0100

    Fin de l'accordeon vue.js

--- without-js/js/Vuejs.js
+++ without-js/js/Vuejs.js
@@ -1,17 +1,38 @@
 new Vue({
     el: '#app',
     data: {
-        screenNameRoot:"img/screen/screen",   
+        isDivAccordeonActive:true,
+        faq: {
+            currentQuestion:0,
+            questions: [
+                {
+                    sentence: 'Started several mistake joy say painful removed reached end',
+                    answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                },
+                {
+                    sentence: 'Although moreover mistaken kindness me feelings do be marianne?',
+                    answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                },
+                {
+                    sentence: 'By in no ecstatic wondered disposal my speaking?',
+                    answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                }
+            ]
+        }
+    },
+    computed: {
+        activeClass: function() {
+            return{
+                active: this.isDivAccordeonActive
+            }
+        },
     },
     methods: {
-        screenName: function(index){
-            console.log(this.screenNameRoot + index + ".jpg");
-            return this.screenNameRoot + index + ".jpg"
-
+        switchActivateAccordeon: function(index) {
+            this.isDivAccordeonActive = !this.isDivAccordeonActive
         }
+        
     }
-
-
 
 
 })
